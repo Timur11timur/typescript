ec2c2ddf96fac0d05d71e0330b5b3c0dbe848e14
Timur <t.fralik@aimprosoft.com>
2021-10-01 16:05:19 +0300

    Add types

--- 1types.js
+++ 1types.js
@@ -1,2 +1,31 @@
-var str = 'Hello';
-console.log(str);
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var message = 'Hello TypeScript';
+var numberArray = [1, 1, 2, 3, 5, 8, 13];
+var numberArray2 = [1, 1, 2, 3, 5, 8, 13];
+var words = ['Hello', 'TypeScript'];
+//Tuple
+var contact = ['Timur', 1234567];
+//Any
+var variable = 42;
+variable = 'New string';
+variable = [];
+//return value
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Heisenberg');
+//Never
+function throwError(message) {
+    throw new Error(message);
+}
+function infinite() {
+    while (true) {
+    }
+}
+var login = 'admin';
+var id1 = 1234;
+var id2 = '1234';
--- 1types.ts
+++ 1types.ts
@@ -1,3 +1,51 @@
-const str: string = 'Hello';
+const isFetching: boolean = true;
+const isLoading: boolean = false;
 
-console.log(str);
+const int: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
+
+const message: string = 'Hello TypeScript';
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13];
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13];
+
+const words: string[] = ['Hello', 'TypeScript'];
+
+//Tuple
+const contact: [string, number] = ['Timur', 1234567];
+
+//Any
+let variable: any = 42;
+variable = 'New string';
+variable = [];
+
+//return value
+function sayMyName(name: string): void {
+    console.log(name);
+}
+sayMyName('Heisenberg');
+
+//Never
+function throwError(message: string): never {
+    throw new Error(message);
+}
+
+function infinite(): never {
+    while (true) {
+
+    }
+}
+
+//Type
+type Login = string;
+
+const login: Login = 'admin';
+//const login2: Login = 2; //Error
+
+type ID = string | number
+const id1: ID = 1234;
+const id2: ID = '1234';
+//const id3: ID = true; //Error
+
+type SomeType = string | null | undefined
